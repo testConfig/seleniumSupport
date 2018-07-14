5858f3b224c32358ba177c7fe145c804ade4b88c
testConfig <ahammedsorif07@gmail.com>
2018-07-14 02:34:47 -0400

    ConnectWithExcel file is edited but not finish yet

--- seleniumSupport/src/main/java/test/seleniumSupport/ConnectWithExcel.java
+++ seleniumSupport/src/main/java/test/seleniumSupport/ConnectWithExcel.java
@@ -1,5 +1,30 @@
 package test.seleniumSupport;
 
-public class ConnectWithExcel {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ConnectWithExcel {
+	XSSFWorkbook xBook;
+	public void getConnection(String FilePath) {
+		
+		try {
+			File xFile = new File(FilePath);
+			FileInputStream input = new FileInputStream(xFile);
+			XSSFWorkbook xBook = new XSSFWorkbook(input);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void getData(int SheetNumber) {
+		XSSFSheet xSheet = xBook.getSheetAt(SheetNumber);
+		
+		
+	}
 }
