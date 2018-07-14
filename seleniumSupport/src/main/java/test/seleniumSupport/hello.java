195e23bff1609ff2581cbba57084ac803019347c
Ahammed sorif <41215775+testConfig@users.noreply.github.com>
2018-07-14 03:36:01 +0000

    new math is added

--- seleniumSupport/src/main/java/test/seleniumSupport/hello.java
+++ seleniumSupport/src/main/java/test/seleniumSupport/hello.java
@@ -3,7 +3,9 @@ package test.seleniumSupport;
 public class hello {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello world how are you");
+		int x = 5
+int y =10			
+		System.out.println("Hello world how are you it is very good file");
+		System.out.println(x*y)
 	}
 }
