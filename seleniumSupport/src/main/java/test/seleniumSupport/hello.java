c682a27489b1bb667702f46661cbb3dc8dab876d
testConfig <ahammedsorif07@gmail.com>
2018-07-13 23:38:40 -0400

    math is corrected

--- seleniumSupport/src/main/java/test/seleniumSupport/hello.java
+++ seleniumSupport/src/main/java/test/seleniumSupport/hello.java
@@ -3,9 +3,9 @@ package test.seleniumSupport;
 public class hello {
 
 	public static void main(String[] args) {
-		int x = 5
-int y =10			
+		int x = 5;
+int y =10;			
 		System.out.println("Hello world how are you it is very good file");
-		System.out.println(x*y)
+		System.out.println((x*y));
 	}
 }
