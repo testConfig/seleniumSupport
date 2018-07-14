fb5493062fda49203931e31e5ce3496b8c7f7414
testConfig <ahammedsorif07@gmail.com>
2018-07-14 00:11:06 -0400

    dfdf

--- seleniumSupport/src/main/java/test/seleniumSupport/hello.java
+++ seleniumSupport/src/main/java/test/seleniumSupport/hello.java
@@ -6,6 +6,10 @@ public class hello {
 		int x = 5;
 int y =10;			
 		System.out.println("Hello world how are you it is very good file");
+<<<<<<< HEAD
 		System.out.println(x*y);
+=======
+		System.out.println((x*y));
+>>>>>>> parent of b9f2eb1... All unImportant file is Deleted
 	}
 }
