cdd31008f262a86743e543ab801ecc4dae9bfaa4
win <ahammedsroif07@gmail.com>
2018-07-21 20:20:51 -0400

    new properties is added

--- seleniumSupport/src/main/java/screenShot/getDataBaseConnection.java
+++ seleniumSupport/src/main/java/screenShot/getDataBaseConnection.java
@@ -1,5 +1,28 @@
 package screenShot;
 
-public class getDataBaseConnection {
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
 
+public class getDataBaseConnection {
+	static	Properties property;
+		public static void loadproperty() {
+			property = new Properties();
+			File xfile = new File(System.getProperty("user.dir")+"//All_Property//Excel.Properties");
+			
+			try {
+				FileReader xRead = new FileReader(xfile);
+				property.load(xRead);
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+			
+		}
+		public static void main(String[] args)
+		{
+			loadproperty();
+			String xdata = property.getProperty("SecondData");
+			System.out.println(xdata);
+		}
 }
